58b348494ced80e5da77fae2cd5854840c6e45b6
Alex Moreton <lxmrtn7@gmail.com>
2026-09-17 11:38:00 +0100

    Update middleware to be proxy as per latest Next conventions and redirect to our todos route

--- proxy.ts
+++ proxy.ts
@@ -1,16 +1,16 @@
 import { getSessionCookie } from "better-auth/cookies";
 import { type NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const sessionCookie = await getSessionCookie(request);
 
   if (!sessionCookie) {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/todos/:path*"],
 };
